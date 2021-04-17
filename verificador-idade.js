7c8055db25f9319b0af0e2fdf80290ba2cae4647
Gabriel Lisboa da Silva <gabriellisboa08@gmail.com>
2021-04-17 11:05:57 -0300

    Fim do programa

--- verificador-idade.js
+++ verificador-idade.js
@@ -1,28 +1,52 @@
 function verificar() {
     let data = new Date()
     let anoAtual = data.getFullYear()
-    let txtano = document.getElementById('txtano')
+    let txtAno = document.getElementById('txtano')
+    let anoNasc = Number(txtAno.value)
     res = document.getElementById('res')
-    if (txtano.value.length == 0 || anoNasc > anoAtual) {
-        window.alert('[ERRO] dados inválidos')
-    } 
-    
-    else {
-        let anoNasc = Number(txtano.value)
+    if ( anoAtual < anoNasc || txtAno.value.length == 0) {
+        window.alert('[ERRO] Dados inválidos')
+    } else {
         let idade = anoAtual - anoNasc 
-        let opcaoSex = document.getElementsByName('Sexo')
         let genero = ''
-        
-        if (opacaoSex[0].checked) {
-            let genero = 'masc'
-            res.innerText = idade
+        let opcaosex = document.getElementsByName('opcaosex')
+        let img = document.createElement('img') // ESSA Função cria um elemento HTML por JS
+        img.setAttribute('id', 'foto') // Aqui foi Atribuído uma ID com o nome de foto ao elemento criado acima
+        if (opcaosex[0].checked) {
+            genero = 'Homem'
+            if (idade >= 0 && idade < 12 ){
+                img.setAttribute('src','criança-m.png')
+                //Criança
+            } else if (idade >= 12 && idade < 21) {
+                img.setAttribute('src', 'jovem-m.png')
+                //Jovem
+            }else if (idade >= 21 && idade < 60) {
+                //Adulto
+                img.setAttribute('src', 'adulto-m.png')
+            }else {
+                //idoso
+                img.setAttribute('src', 'idoso-m.png')
+            }
+      
+        }else {
+            genero = 'Mulher'
+            if (idade >= 0 && idade < 12 ){
+                img.setAttribute('src' , 'Criança-f.png')
+                //Criança
+            } else if (idade >= 12 && idade < 21) {
+                //Jovem
+                img.setAttribute('src','jovem-f.png')
+            }else if (idade >= 21 && idade < 60) {
+                //Adulto
+                img.setAttribute('src' , 'adulto-f.png')
+            }else { 
+                //idoso
+                img.setAttribute('src', 'idoso-f.png')
+            }
         }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Foi detectado ${genero} com ${idade} ano(s)`
+        res.appendChild(img)
     }
     
-    
-    
-    
-
-   
-
 }
